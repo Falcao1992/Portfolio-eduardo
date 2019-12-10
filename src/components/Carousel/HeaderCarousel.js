114c943be4bd05b143fb25e3f499c8f8a2424701
Falcao1992 <eduardo.marselle@gmail.com>
2019-12-10 09:27:26 +0100

    change

--- src/components/Carousel/HeaderCarousel.js
+++ src/components/Carousel/HeaderCarousel.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import "./HeaderCarousel.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./HeaderCarousel.css";
 import { Carousel } from "react-responsive-carousel";
 
 class HeaderCarousel extends Component {
@@ -8,12 +8,12 @@ class HeaderCarousel extends Component {
     return (
       <Carousel
         autoPlay={true}
-        interval={6000}
+        interval={8000}
         infiniteLoop={true}
         showArrows={true}
         showThumbs={false}
         showStatus={false}
-        transitionTime={1000}
+        transitionTime={2000}
         showIndicators={false}
       >
         <div className="slide-container">
@@ -22,23 +22,23 @@ class HeaderCarousel extends Component {
             src="https://previews.123rf.com/images/scyther5/scyther51605/scyther5160500017/56962905-photographe-appareil-photo-moniteur-moniteur-design-ordinateur-portable-photo-%C3%A9cran-photographie-stock.jpg"
             alt="Bureau Desing"
           />
-          <p className="legend">Legend 1</p>
+          <p className="legend-slide">voici la legende numero 1</p>
         </div>
         <div className="slide-container">
           <img
             className="slide__img kenburns-top-right"
-            src="https://st3.depositphotos.com/1607243/17782/i/1600/depositphotos_177826624-stock-photo-computer-magazine-design-screen-modern.jpg"
+            src="git push --set-upstream origin feature/displayProject"
             alt="Bureau Blanc"
           />
-          <p className="legend">Legend 2</p>
+          <p className="legend-slide">voici la Legend 2</p>
         </div>
         <div className="slide-container">
           <img
             className="slide__img kenburns-top-right"
             src="https://tmv.tmvtours.fr/wp-content/uploads/sites/tours/NEWS_WILDSCHOOL.jpg"
             alt="Wild Code School"
-         />
-          <p className="legend">Legend 3</p>
+          />
+          <p className="legend-slide"> voici la Legend 3</p>
         </div>
       </Carousel>
     );
