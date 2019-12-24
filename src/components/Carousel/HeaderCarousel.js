54162d4cf7bbf08c208b15cf02daa60a39aa1c9d
Falcao1992 <eduardo.marselle@gmail.com>
2019-12-24 23:30:57 +0100

    systeme de grille pour les projets terminé

--- src/components/Carousel/HeaderCarousel.js
+++ src/components/Carousel/HeaderCarousel.js
@@ -6,7 +6,7 @@ import { Carousel } from "react-responsive-carousel";
 class HeaderCarousel extends Component {
   render() {
     return (
-      <Carousel
+      <Carousel className="constainer-caroussel"
         autoPlay={true}
         interval={8000}
         infiniteLoop={true}
@@ -31,7 +31,7 @@ class HeaderCarousel extends Component {
             src="https://www.alain-bensoussan.com/wp-content/uploads/2017/05/34226189.jpg"
             alt="Bureau Blanc"
           />
-          <p className="legend-slide tracking-in-contract-bck">bienvenue sur mon portfolio
+          <p className="legend-slide tracking-in-contract-bck">mes projets
           </p>
         </div>
         <div className="slide-container">
